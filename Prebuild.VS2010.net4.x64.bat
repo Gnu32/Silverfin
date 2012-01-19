b8ff2094046f78e20aea113f508f284b1a3d0b9f
Major Rasputin <simplaza@gmail.com>
2012-01-19 17:24:10 +0000

    - Turned all the Prebuild batch files into a supermegawesome batchfile wizard
    Doing a test push into the repo. Current revision is broken as I've not dived into the source yet to fix all the renames.
    
    Let's see if git actually works.

--- Prebuild.VS2010.net4.x64.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-Tools\Prebuild.exe /target vs2010 /targetframework v4_0 
-del Compile.*.bat
-echo C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild /p:Platform="x64" > Compile.VS2010.net4.x64.bat /p:DefineConstants=ISWIN
-pause
